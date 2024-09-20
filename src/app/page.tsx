b7acf7406ac6577d3dd575fcb57dd7dfbcc08cf6
obedvs <is.paulvega.17102@gmail.com>
2024-09-20 08:02:29 -0600

    Update Dependencies
    And Styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
 import Hero from "@/components/hero";
+import Pricing from "@/components/pricing";
+import Roadmap from "@/components/roadmap";
 import Services from "@/components/services";
+import Testimonial from "@/components/testimonial";
 
 const LandingPage = () => {
   return (
     <main className="w-full grid">
       <Hero />
       <Services />
+      <Testimonial />
+      <Pricing />
+      <Roadmap />
     </main>
   );
 };
