52dd80d859f4cba227cf1a94ccc83aa6a2533c5b
obedvs <is.paulvega.17102@gmail.com>
2024-09-20 08:00:45 -0600

    Add Services Section

--- src/components/services.tsx
+++ src/components/services.tsx
@@ -3,14 +3,14 @@ import Image from "next/image";
 const Services = () => {
   return (
     <section className="bg-[#90EDFF]/30">
-      <div className="relative grid grid-cols-1 md:grid-cols-2 container mx-auto py-12">
-        <article className="relative min-h-[560px] flex justify-center">
-          <Image src="/services_1.png" width={560} height={560} alt="Servicios" className="h-full w-auto" />
-        </article>
-        <article className="space-y-4">
-          <h2 className="text-2xl font-bold tracking-tight sm:text-4xl">
+      <div className="relative grid grid-cols-1 md:grid-cols-2 container mx-auto py-12 px-2">
+          <h2 className="text-2xl font-bold tracking-tight sm:text-4xl col-span-2 text-center mb-8">
             Nuestros Servicios
           </h2>
+        <article className="relative min-h-[560px] md:flex justify-center hidden">
+          <Image src="/services_1.png" width={560} height={560} alt="Servicios" className="h-full w-auto" />
+        </article>
+        <article className="flex flex-col items-start gap-y-4">
           <h3 className="text-lg font-semibold tracking-tight sm:text-xl">Clinicas</h3>
           <p>Ofrecemos servicios de clinicas</p>
           <button className="bg-[#34A5AB] py-2 px-4 rounded-3xl font-medium text-white text-xl">
